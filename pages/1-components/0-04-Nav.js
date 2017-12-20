a0cf374dc48f28eba4ba33b85f8f0446c5d189a7
minooo <552594996@qq.com>
2017-12-20 18:02:56 +0800

    s

--- pages/1-components/0-04-Nav.js
+++ pages/1-components/0-04-Nav.js
@@ -1,1 +1,19 @@
 import React from 'react'
+import { ActiveLink } from '@components'
+
+const config = [
+  { ico: 'i-home', text: '首页' },
+  { ico: 'i-loan', text: '贷款' },
+  { ico: 'i-card', text: '信用卡' },
+  { ico: 'i-user', text: '我的' },
+]
+
+export default () => (
+  <div style={{ position: 'relative', zIndex: 5 }} className="flex h110 ai-stretch bg-white border-top-shadow">
+    {
+      config.map(item => ({
+
+      }))
+    }
+  </div>
+)
